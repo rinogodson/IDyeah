05193939976f0bef56889c55951e06f7eb0b1ada
rinogodson <rinogodson@gmail.com>
2025-05-05 21:22:45 +0530

    Server Updates

--- backend/go.mod
+++ backend/go.mod
@@ -3,9 +3,14 @@ module idyeah/backend
 go 1.24.1
 
 require (
+	github.com/gofiber/fiber/v3 v3.0.0-beta.4
+	gorm.io/driver/sqlite v1.5.7
+	gorm.io/gorm v1.26.0
+)
+
+require (
 	github.com/andybalholm/brotli v1.1.1 // indirect
 	github.com/fxamacker/cbor/v2 v2.8.0 // indirect
-	github.com/gofiber/fiber/v3 v3.0.0-beta.4 // indirect
 	github.com/gofiber/schema v1.3.0 // indirect
 	github.com/gofiber/utils/v2 v2.0.0-beta.8 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -19,12 +24,9 @@ require (
 	github.com/tinylib/msgp v1.2.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.61.0 // indirect
-	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/net v0.39.0 // indirect
 	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
-	gorm.io/driver/sqlite v1.5.7 // indirect
-	gorm.io/gorm v1.26.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -10,16 +10,16 @@ import (
 
 type Student struct {
 	gorm.Model
-	Name     string
-	AdmNo    string
-	PhNo     string
-	Guardian string
-	Address  string
-	Dob      string
-	Blood    string
-	Boarding string
-	Bus      string
-	Club     string
+	Name     string `json:"name"`
+	AdmNo    string `json:"adm"`
+	PhNo     string `json:"ph"`
+	Guardian string `json:"guardian"`
+	Address  string `json:"address"`
+	Dob      string `json:"dob"`
+	Blood    string `json:"blood"`
+	Boarding string `json:"bp"`
+	Bus      string `json:"bus"`
+	Club     string `json:"club"`
 	Image    string
 }
 
@@ -36,8 +36,33 @@ func main() {
 	}
 
 	app := fiber.New()
+
 	app.Get("/", func(c fiber.Ctx) error {
 		return c.SendString("Hello World, this is a test backend for IDyeah.")
+	})
+
+	app.Post("/add", func(c fiber.Ctx) error {
+		var s Student
+		if err := c.Bind().JSON(&s); err != nil {
+			return c.Status(fiber.StatusBadRequest).SendString("Failed to parse body" + err.Error())
+		}
+		db.Create(&Student{Name: s.Name, AdmNo: s.AdmNo, PhNo: s.PhNo, Guardian: s.Guardian, Address: s.Address, Dob: s.Dob, Club: s.Club, Image: "", Blood: s.Blood, Boarding: s.Boarding, Bus: s.Bus})
+		return c.SendString("Value Added to the DB!")
+	})
+
+	app.Post("/edit", func(c fiber.Ctx) error {
+		type ReqType struct {
+			From int64   `json:"from"`
+			To   Student `json:"to"`
+		}
+		var s ReqType
+		if err := c.Bind().JSON(&s); err != nil {
+			return c.Status(fiber.StatusBadRequest).SendString("Check the format")
+		}
+		var val Student
+		db.First(&val, s.From)
+		db.Model(&s).Update("name", s.To.Name)
+		return c.SendString("Changes Made")
 	})
 
 	app.Get("/api/", func(c fiber.Ctx) error {
